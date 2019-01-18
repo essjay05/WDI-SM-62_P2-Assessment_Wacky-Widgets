ca1092b6fe2bec777993d6322c4e9c0e7966268b
Joy <stepjoyous@gmail.com>
2019-01-18 13:11:42 -0800

    Created Widgets controller with index show new create delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'widgets/index'
+  get 'widgets/show'
+  get 'widgets/new'
+  get 'widgets/create'
+  get 'widgets/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
