d51072098f9cf198ebebf4b827fc0e81cffab4f5
Joy <stepjoyous@gmail.com>
2019-01-18 15:51:21 -0800

    completed exam

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,16 +1,41 @@
 class WidgetsController < ApplicationController
+  before_action :set_widget, only: [:show, :destroy]
   def index
+    @widgets = Widget.all
   end
 
   def show
+    set_widget
   end
 
   def new
+    @widget = Widget.new
   end
 
   def create
+    @widget = Widget.new(widget_params)
+
+    if @widget.save
+      redirect_to widgets_path
+    else
+      render :new
+    end
   end
 
-  def delete
+  def destroy
+    
+    @widget.destroy
+    redirect_to widgets_path
+    
   end
+
+  private
+  def widget_params
+    params.require(:widget).permit(:id, :description, :quantity)
+  end
+  def set_widget
+    
+    @widget = Widget.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'widgets/index'
-  get 'widgets/show'
-  get 'widgets/new'
-  get 'widgets/create'
-  get 'widgets/delete'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # NEW
+  root to: "widgets#new"
+  # INDEX
+  get 'widgets/' => 'widgets#index'
+  # CREATE
+  post 'widgets/' => 'widgets#create'
+  #DESTROY
+  delete '/widgets/:id' => 'widgets#destroy'
 end
